9df75a9a6c260a7c530f729ede80ed89d3ee7bf5
Gary Abramson <gary.abramson@gmail.com>
2017-10-09 15:38:22 -0500

    Basic project structure

---
+++ MinCutTest/MinCutTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinCutterLib;
+
+namespace MinCutTest
+{
+    [TestClass]
+    public class MinCutTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var minCutter = new MinCutter();
+        }
+    }
+}
