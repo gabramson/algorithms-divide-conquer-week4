74a637027b7250a5338bfefebeacd7e058c8b0f0
Gary Abramson <gary.abramson@gmail.com>
2017-10-09 15:06:16 -0500

    initial

---
+++ MinCut/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinCut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
