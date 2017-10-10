adc22717fe4bf4dbcc512580328602e5965cd6b4
Gary Abramson <gary.abramson@gmail.com>
2017-10-10 16:55:19 -0500

    Submission successful

--- MinCut/Program.cs
+++ MinCut/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MinCutterLib;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace MinCut
 {
@@ -6,7 +9,26 @@ namespace MinCut
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var minCutter = new MinCutter(40000);
+
+            Regex regex = new Regex(@"^(\d+)(\t(\d+))+");
+            string line;
+            using (StreamReader srStreamRdr = new StreamReader(@"kargerMinCut.txt"))
+            {
+                while ((line = srStreamRdr.ReadLine()) != null)
+                {
+                    var x = regex.Matches(line);
+                    int first = int.Parse(x[0].Groups[1].Captures[0].Value);
+                    for (int i = 0; i < x[0].Groups[3].Captures.Count; i += 1)
+                    {
+                        int second = int.Parse(x[0].Groups[3].Captures[i].Value);
+                        minCutter.AddEdge(first, second);
+                    }
+                }
+            }
+            minCutter.Execute();
+            Console.WriteLine(minCutter.Crossings);
+            Console.ReadKey();
         }
     }
 }
