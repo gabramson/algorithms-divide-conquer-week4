75462bdea4ec17ac5d31e9b96d6980be4e7762cd
Gary Abramson <gary.abramson@gmail.com>
2017-10-09 22:24:15 -0500

    MinCutter implemented up to MergeVertices

--- MinCutTest/MinCutTest.cs
+++ MinCutTest/MinCutTest.cs
@@ -7,9 +7,39 @@ namespace MinCutTest
     public class MinCutTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMinCutter()
         {
             var minCutter = new MinCutter();
+            minCutter.AddEdge(1, 2);
+            minCutter.AddEdge(1, 3);
+            minCutter.AddEdge(1, 4);
+            minCutter.AddEdge(1, 7);
+            minCutter.AddEdge(2, 1);
+            minCutter.AddEdge(2, 3);
+            minCutter.AddEdge(2, 4);
+            minCutter.AddEdge(3, 1);
+            minCutter.AddEdge(3, 2);
+            minCutter.AddEdge(3, 4);
+            minCutter.AddEdge(4, 1);
+            minCutter.AddEdge(4, 2);
+            minCutter.AddEdge(4, 3);
+            minCutter.AddEdge(4, 5);
+            minCutter.AddEdge(5, 4);
+            minCutter.AddEdge(5, 6);
+            minCutter.AddEdge(5, 7);
+            minCutter.AddEdge(5, 8);
+            minCutter.AddEdge(6, 5);
+            minCutter.AddEdge(6, 7);
+            minCutter.AddEdge(6, 8);
+            minCutter.AddEdge(7, 1);
+            minCutter.AddEdge(7, 5);
+            minCutter.AddEdge(7, 6);
+            minCutter.AddEdge(7, 8);
+            minCutter.AddEdge(8, 5);
+            minCutter.AddEdge(8, 6);
+            minCutter.AddEdge(8, 7);
+            minCutter.Execute();
+            Assert.AreEqual(2, minCutter.Crossings);
         }
     }
 }
--- MinCutter/MinCutter.cs
+++ MinCutter/MinCutter.cs
@@ -1,8 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MinCutterLib
 {
     public class MinCutter
     {
+        private Dictionary<int, Vertex> Vertices = new Dictionary<int, Vertex>();
+        private int bestCrossings = int.MaxValue;
+        public int Crossings { private set; get; }
+
+        public void AddEdge(int first, int second)
+        {
+            if (!Vertices.ContainsKey(first))
+            {
+                Vertices.Add(first, new Vertex(first));
+            }
+            if (!Vertices.ContainsKey(second))
+            {
+                Vertices.Add(second, new Vertex(second));
+            }
+            if (!Vertices[first].OutNeighbors.ContainsKey(second)){
+                Vertices[first].OutNeighbors.Add(second, 1);
+                Vertices[second].OutNeighbors.Add(first, 1);
+            }
+        }
+
+        public void Execute()
+        {
+            Crossings = 0;
+
+            MergeVertices(1, 2);
+        }
+
+        private void MergeVertices(int first, int second)
+        {
+            foreach(var index in Vertices[second].OutNeighbors.Keys){
+                if (index != first)
+                {
+                    if (Vertices[first].OutNeighbors.ContainsKey(index))
+                    {
+                        Vertices[first].OutNeighbors[index] += Vertices[first].OutNeighbors[index];
+                    }
+                }
+            }
+            Vertices[first].OutNeighbors.Remove(second);
+            Vertices.Remove(second);
+        }
+
+        private struct Vertex
+        {
+            public Vertex(int index)
+            {
+                Index = index;
+                OutNeighbors = new Dictionary<int , int>();
+            }
+
+            public int Index { private set; get; }
+            public Dictionary<int , int > OutNeighbors { set; get; }
+        }
     }
 }
